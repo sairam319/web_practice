dcbd83e835463616f06b9d2028d55140493dac64
sairam319 <sairam309@gmail.com>
2023-06-27 11:24:38 +0530

    Conuter_app

--- JavaScript/Counter/index.js
+++ JavaScript/Counter/index.js
@@ -1,17 +1,25 @@
-let countEl= document.getElementById("counter")   //here we get the element that only finds the count   
+let countEl= document.getElementById("counter");  // we get the element that only finds the count   
+
+let saveEl = document.getElementById("save-el");  // saving the content in paragraph tag with id save-el
 
 let count = 0;
 
-function increment(){    //here the increment count is from the onclick attribute of the button tag 
+function increment(){    // the increment count is from the onclick attribute of the button tag 
       
-    count = count + 1 ;
-    countEl.innerText = count ;      
+    count += 1 ;         //on clicking the increment the count get incremented by 1
+    countEl.textContent = count ;  // the count will be added to inner text so that it get displayed
 }
 
 
 function save(){        //here the save functions logsout and shows us the count.
-
-     console.log(count);
-     alert(count);
- 
+    
+     let countStr = count + " - ";   // count along with seperator is assigned to variable 
+     saveEl.textContent += countStr; //rendering variable in saveEl using innertext 
+     countEl.textContent = 0;  // setting up the count to 0
+     count = 0;
 }
+
+
+
+
+
