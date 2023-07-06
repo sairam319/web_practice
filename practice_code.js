2b1a1655b0188990199dc04f2eb85db35cf7c3a9
sairam319 <sairam309@gmail.com>
2023-07-06 12:07:20 +0530

    practice_code

---
+++ practice_code.js
@@ -0,0 +1,32 @@
+let person = "krishna";
+let greeting = "Namasthe ";
+let mypoint = 3;
+
+function callingKrishnaa(){
+
+    console.log(greeting + ", " + person + " !");
+}
+
+callingKrishnaa()
+
+/**Above is pratice program to concatinate strings */
+function add3point(){
+
+    mypoint +=3;
+}
+
+function remove1point(){
+
+    mypoint -=1;
+}
+
+add3point()
+add3point()
+add3point()
+remove1point()
+remove1point()
+
+console.log(mypoint); 
+
+/**Above is to set mypoint to 10 incrementing by 3 decrementing by 1
+ */
