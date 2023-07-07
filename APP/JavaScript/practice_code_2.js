693a63ba7fd91920e737173badd385725436c232
sairam319 <sairam309@gmail.com>
2023-07-07 12:21:10 +0530

    practice_code_2

--- APP/JavaScript/practice_code_2.js
+++ APP/JavaScript/practice_code_2.js
@@ -1,6 +1,27 @@
 let num1 = 9;
 let num2 = 5;
 
-document.getElementById("num1-el") = num1;
-document.getElementById("num2-el") = num2;
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
 
+function addNumber(){
+          
+    console.log("clicked add");
+
+}
+
+function subtractNumber(){
+
+    
+}
+
+function multiplyNumber(){
+
+    
+
+}
+
+function divideNumber(){
+
+    
+}
