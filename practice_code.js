a5bfca0d1ca06cb5190255979cd92c9f8c21e9ed
sairam319 <sairam309@gmail.com>
2023-07-07 10:31:47 +0530

    practice

--- practice_code.js
+++ practice_code.js
@@ -1,32 +1,44 @@
-let person = "krishna";
-let greeting = "Namasthe ";
-let mypoint = 3;
+// let person = "krishna";
+// let greeting = "Namasthe ";
+// let mypoint = 3;
 
-function callingKrishnaa(){
+// function callingKrishnaa(){
 
-    console.log(greeting + ", " + person + " !");
-}
+//     console.log(greeting + ", " + person + " !");
+// }
 
-callingKrishnaa()
+// callingKrishnaa()
 
-/**Above is pratice program to concatinate strings */
-function add3point(){
+// /**Above is pratice program to concatinate strings */
+// function add3point(){
 
-    mypoint +=3;
-}
+//     mypoint +=3;
+// }
 
-function remove1point(){
+// function remove1point(){
 
-    mypoint -=1;
-}
+//     mypoint -=1;
+// }
 
-add3point()
-add3point()
-add3point()
-remove1point()
-remove1point()
+// add3point()
+// add3point()
+// add3point()
+// remove1point()
+// remove1point()
 
-console.log(mypoint); 
+// console.log(mypoint); 
 
 /**Above is to set mypoint to 10 incrementing by 3 decrementing by 1
  */
+
+// let errorParagraph = document.getElementById("error");
+
+
+// function purchase(){
+
+    
+//     errorParagraph.textContent = "Something went wrong , please try again later ."
+// }
+
+
+/** Above JS is display the error message when button is clicked */
